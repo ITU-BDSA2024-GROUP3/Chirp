6d5beee65ccd6db67f350b9616854dbd9aab5b2c
David Nicholas Nielsen <thedavidnn@gmail.com>
2024-08-31 09:44:50 +0200

    Implement reading of CSV file
    Co-authored-by: Theresia <Theresia.ravn@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Text.RegularExpressions;
+
+const string dataPath = "./chirp_cli_db.csv";
+
+if (args[0] == "read")
+{
+    StreamReader reader = new StreamReader(dataPath);
+    reader.ReadLine();
+    while (!reader.EndOfStream)
+    {
+        // Regex taken from: https://stackoverflow.com/questions/6542996/how-to-split-csv-whose-columns-may-contain-comma
+        string[] words = Regex.Split(reader.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+        long unixTimeSeconds = long.Parse(words[2]);
+        DateTimeOffset timeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimeSeconds);
+        string time = $"{timeOffset.Day}/{timeOffset.Month}/{timeOffset.Year}";
+        Console.WriteLine($"{words[0]} @ {time} : {words[1]}");
+    }
+    reader.Close();
+}
+else if(args[0] == "cheep")
+{
+    //dat
+    
+    
+}
