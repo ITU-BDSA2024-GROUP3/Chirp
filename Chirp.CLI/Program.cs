d3198704bc9480ea1446578cd88f2dad44249d25
Adrian <adrianjuul123@gmail.com>
2024-09-04 14:16:47 +0200

    Implemented using for streamreader
    Implemented using streamwriter for more consistent code.
    
    Co-authored-by: Bárður Berg <bardurndberg@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -6,18 +6,18 @@ const string dataPath = "./chirp_cli_db.csv";
 
 if (args[0] == "read")
 {
-    StreamReader reader = new StreamReader(dataPath);
-    reader.ReadLine();
-    while (!reader.EndOfStream)
+    using (StreamReader reader = new StreamReader(dataPath))
     {
-        // Regex taken from: https://stackoverflow.com/questions/6542996/how-to-split-csv-whose-columns-may-contain-comma
-        string[] words = Regex.Split(reader.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-        long unixTimeSeconds = long.Parse(words[2]);
-        DateTimeOffset timeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimeSeconds);
-        string time = $"{timeOffset.Day}/{timeOffset.Month}/{timeOffset.Year}";
-        Console.WriteLine($"{words[0]} @ {time} : {words[1]}");
+        while (!reader.EndOfStream)
+        {
+            // Regex taken from: https://stackoverflow.com/questions/6542996/how-to-split-csv-whose-columns-may-contain-comma
+            string[] words = Regex.Split(reader.ReadLine(), ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            long unixTimeSeconds = long.Parse(words[2]);
+            DateTimeOffset timeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTimeSeconds);
+            string time = $"{timeOffset.Day}/{timeOffset.Month}/{timeOffset.Year}";
+            Console.WriteLine($"{words[0]} @ {time} : {words[1]}");
+        }
     }
-    reader.Close();
 }
 else if(args[0] == "cheep")
 {
