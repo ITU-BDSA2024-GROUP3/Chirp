476f3171b4e931179109f9255a0ad0e516e97e6e
Adrian <adrianjuul123@gmail.com>
2024-09-06 11:24:51 +0200

    Refactored cheepmanager to CSVDatabase
    I refactored the code in cheepmanager to work in CSVDatabase. I changed all places that refrenced cheepmanager to the new class and function in CSVDatabase.
    Co-authored-by: Bárður Berg <bardurndberg@gmail.com>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -2,84 +2,25 @@
 using CsvHelper.Configuration;
 using System.Globalization;
 using System.Text;
-
-
-//Structure of a cheep corrosponding to headers of CSV file
-public record Cheep(string Author, string Message, long Timestamp);
-
-
-//CheepManager does what it says
-public class CheepManager
-{
-    //idk much about readonly, rider just said it would be good
-    private readonly string _dataPath;
-    private readonly CsvConfiguration _csvConfig;
-
-    public CheepManager(string dataPath)
-    {
-        _dataPath = dataPath;
-        //set the config to "InvariantCulture" and inform the program that the file already has headers
-        _csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
-        {
-            HasHeaderRecord = true,
-        };
-    }
-
-    //again, self explanatory
-    public void ReadCheeps()
-    {
-        //ensure file exists
-        if (!File.Exists(_dataPath))
-        {
-            Console.WriteLine("Data file does not exist.");
-            return;
-        }
-        //create streamreader and CSVreader with "using"
-        using (var reader = new StreamReader(_dataPath))
-        using (var csv = new CsvReader(reader, _csvConfig))
-        {
-            //assaign a recod of cheeps and loop through them
-            var cheeps = csv.GetRecords<Cheep>(); 
-            foreach (var cheep in cheeps)
-            {
-                DateTimeOffset timeOffset = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp);
-                string time = $"{timeOffset.Day}/{timeOffset.Month}/{timeOffset.Year}";
-                Console.WriteLine($"{cheep.Author} @ {time} : {cheep.Message}");
-            }
-        }
-    }
-    
-    //here we go again
-    public void AddCheep(string message)
-    {
-        //create new cheep record
-        var newCheep = new Cheep(Environment.UserName, message, ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds());
-        
-        //create streamwriter and CSVwriter with using
-        using (var writer = new StreamWriter(_dataPath, append: true))
-        using (var csv = new CsvWriter(writer, _csvConfig))
-        {
-            //add cheep to file then add blank character to end
-            csv.WriteRecord(newCheep);
-            writer.WriteLine();
-        }
-    }
-}
+using SimpleDB;
 
 class Program
 {
     static void Main(string[] args)
     {
         const string dataPath = "./chirp_cli_db.csv";
-        var cheepManager = new CheepManager(dataPath);
+        var cheepManager = new CSVDatabase<Cheep>(dataPath);
 
         if (args[0] == "read")
         {
-            cheepManager.ReadCheeps();
+            foreach (var cheep in cheepManager.Read())
+            {
+                Console.WriteLine(cheep);
+            }
         }
         else if (args[0] == "cheep")
         {
-            cheepManager.AddCheep(args[1]);
+            cheepManager.Store(new Cheep(Environment.UserName, args[1], ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds()));
         }
         else
         {
--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,26 +1,58 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Globalization;
 using CsvHelper;
+using CsvHelper.Configuration;
+using ArrayList = System.Collections.ArrayList;
 
 namespace SimpleDB;
 
-public class Cheep
-{
-    public string Author { get; set; }
-    public string Message { get; set; }
-    public long Timestamp { get; set; }
-}
+public record Cheep(string Author, string Message, long Timestamp);
+
 
 public class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    //Reads from the database
-    public IEnumerable<T> Read(int? limit = null)
+    //idk much about readonly, rider just said it would be good
+    private readonly string _dataPath;
+    private readonly CsvConfiguration _csvConfig;
+
+    public CSVDatabase(string dataPath)
     {
-        return null;
+        _dataPath = dataPath;
+        //set the config to "InvariantCulture" and inform the program that the file already has headers
+        _csvConfig = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true,
+        };
     }
 
-    //Stores data in the database
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        //ensure file exists
+        if (!File.Exists(_dataPath))
+        {
+            Console.WriteLine("Data file does not exist.");
+            return null;
+        }
+        
+        //create streamreader and CSVreader with "using"
+        using (var reader = new StreamReader(_dataPath))
+        using (var csv = new CsvReader(reader, _csvConfig))
+        {
+            //assaign a record of cheeps and return them
+            var dataRecords = csv.GetRecords<T>().ToList();
+            return dataRecords;
+        }
+    }
+    
     public void Store(T record)
     {
-        
+        //create streamwriter and CSVwriter with using
+        using (var writer = new StreamWriter(_dataPath, append: true))
+        using (var csv = new CsvWriter(writer, _csvConfig))
+        {
+            //add cheep to file then add blank character to end
+            csv.WriteRecord(record);
+            writer.WriteLine();
+        }
     }
 }
