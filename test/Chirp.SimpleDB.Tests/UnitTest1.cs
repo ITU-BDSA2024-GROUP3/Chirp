2edfe1084baddc4378e0fe05a74e00664725a413
Bárður Berg <bardurndberg@gmail.com>
2024-09-13 12:09:00 +0200

    forgor to git add
    Co-authored-by: Nickie <nalm@itu.dk>

--- test/Chirp.SimpleDB.Tests/UnitTest1.cs
+++ test/Chirp.SimpleDB.Tests/UnitTest1.cs
@@ -2,9 +2,35 @@ namespace Chirp.SimpleDB.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    public string dataPath ="data/TestData.csv";
+    
+    var cheepManager = new CSVDatabase<Cheep>(dataPath);
+        
+    [Theory]
+    public void TestMessage(string message, long unixTimeStamp)
     {
-
+        
+        cheepManager.Store(new Cheep(Environment.UserName, message, unixTimeStamp));
+        
+        Assert.Equal(message , cheepManager.Read.Last().Message);
+        
+    }
+    
+    public void TestAuthor(string message, long unixTimeStamp)
+    {
+        
+        cheepManager.Store(new Cheep(Environment.UserName, message, unixTimeStamp));
+        
+        Assert.Equal(Environment.UserName , cheepManager.Read.Last().Author);
+        
+    }
+    
+    public void TestTimeStamp(string message, long unixTimeStamp)
+    {
+        
+        cheepManager.Store(new Cheep(Environment.UserName, message, unixTimeStamp));
+        
+        Assert.Equal(unixTimeStamp , cheepManager.Read.Last().Timestamp);
+        
     }
 }
