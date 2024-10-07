0989cab9de1f5feeaffc9bb13909a83520bc3a98
Theresia <theresia.ravn@gmail.com>
2024-10-07 11:13:38 +0200

    testing unit timestamp

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -3,10 +3,11 @@ namespace UnitTest;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void convertunixtimestamp()
     {
-        //comment
-        Assert.True(true);
+        int timestamp = 1728284672;
+        String time = CheepService.UnixTimeStampToDateTimeString(timestamp);
+        Assert.Equal(time, "10/07/24 7.04.32");
         
     }
 }
