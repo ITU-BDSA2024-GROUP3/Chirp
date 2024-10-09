d2c283b9e7132ca0114210b56c609c2840b2c276
nalm2812 <nalm@itu.dk>
2024-10-09 15:11:39 +0200

    template for inmemoryDB
    Co-authored-by: Theresia <theresia.ravn@gmail.com>

---
+++ UnitTest/UnitTestCheepRepo.cs
@@ -0,0 +1,25 @@
+﻿using Chirp.Razor;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace UnitTest;
+
+public class UnitTestCheepRepo
+{
+    [Fact]
+    public async void convertunixtimestamp()
+    {
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+
+        using var context = new ChirpDBContext(builder.Options);
+        await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+
+        ICheepRepository repository = new CheepRepository(context);
+
+// Act
+        var result = repository.ReadCheeps(1, 1);
+        
+    }
+}
