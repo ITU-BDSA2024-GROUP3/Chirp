cc27448f36ab9e4e1a301e701510f7e40a97023e
nalm2812 <nalm@itu.dk>
2024-10-11 11:50:19 +0200

    minor changes to test

--- UnitTest/UnitTestCheepRepo.cs
+++ UnitTest/UnitTestCheepRepo.cs
@@ -1,4 +1,5 @@
 ﻿using Chirp.Razor;
+using Chirp.Razor.DomainModel;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,8 +11,20 @@ public class UnitTestCheepRepo
    public async void TestReadCheep()
    {
       var repo = await UtilFunctionsTest.CreateInMemoryDb();
-      var cheep = await repo.ReadCheeps(1, null);
-      Assert.NotNull(cheep);
+      var cheeps = await repo.ReadCheeps(1, null);
+      Assert.NotNull(cheeps);
+      var messages = new List<string>();
+      foreach (CheepDTO cheep in cheeps)
+      {
+         messages.Add(cheep.Text);
+      }
+      Assert.True(messages.Contains("They were married in Chicago, with old Smith, and was expected aboard every day; meantime, the two went past me"));
    }
+   /*[Theory]
+   public async void TestReadCheepText()
+   {
+      
+   }*/
+   
     
 }
