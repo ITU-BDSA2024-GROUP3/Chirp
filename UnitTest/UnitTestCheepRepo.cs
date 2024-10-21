f4861cab8c76a3daf2aa42d398f94d3b61790cac
Theresia <theresia.ravn@gmail.com>
2024-10-20 22:39:57 -0400

    added test create cheep- the async/await functions is missing in order to test the cheep is created and added to database

--- UnitTest/UnitTestCheepRepo.cs
+++ UnitTest/UnitTestCheepRepo.cs
@@ -9,7 +9,7 @@ namespace UnitTest;
 
 public class UnitTestCheepRepo : IDisposable
 {
-   
+
 
    public async void Dispose()
    {
@@ -25,16 +25,17 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-        
+
       var author = new Author() { AuthorId = 1, Cheeps = null, Email = "mymail", Name = "Tom" };
-        
+
       var cheep = new Cheep
       {
          CheepId = 1,
          Author = author,
          AuthorId = author.AuthorId,
          Text = "messageData",
-         TimeStamp = DateTimeOffset.FromUnixTimeSeconds(1728643569).UtcDateTime // Ensure this matches the format in your model
+         TimeStamp = DateTimeOffset.FromUnixTimeSeconds(1728643569)
+            .UtcDateTime // Ensure this matches the format in your model
       };
 
       context.Authors.Add(author);
@@ -48,12 +49,13 @@ public class UnitTestCheepRepo : IDisposable
       {
          messages.Add(cheeP.Text);
       }
+
       //Assert.True(messages.Contains("messageData"));
       Assert.Contains("messageData", messages);
    }
-   
+
    [Theory]
-   [InlineData(1,"Tom", "myemail")]
+   [InlineData(1, "Tom", "myemail")]
    public async void TestReadAuthorById(int id, string name, string email)
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
@@ -63,19 +65,20 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-        
+
       var author = new Author() { AuthorId = id, Cheeps = null, Email = email, Name = name };
-      
+
       context.Authors.Add(author);
       await context.SaveChangesAsync();
       ICheepRepository repo = new CheepRepository(context);
-      
+
       var authorDto = await repo.ReadAuthorById(id);
       Assert.NotNull(author);
       Assert.Equal(name, authorDto.Name);
    }
+
    [Theory]
-   [InlineData(1,"Tom", "myemail")]
+   [InlineData(1, "Tom", "myemail")]
    public async void TestReadAuthorByEmail(int id, string name, string email)
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
@@ -85,19 +88,20 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-        
+
       var author = new Author() { AuthorId = id, Cheeps = null, Email = email, Name = name };
-      
+
       context.Authors.Add(author);
       await context.SaveChangesAsync();
       ICheepRepository repo = new CheepRepository(context);
-      
+
       var authorDto = await repo.ReadAuthorByEmail(email);
       Assert.NotNull(author);
       Assert.Equal(name, authorDto.Name);
    }
+
    [Theory]
-   [InlineData(1,"Tom", "myemail")]
+   [InlineData(1, "Tom", "myemail")]
    public async void TestReadAuthorByName(int id, string name, string email)
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
@@ -107,20 +111,20 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-        
+
       var author = new Author() { AuthorId = id, Cheeps = null, Email = email, Name = name };
-      
+
       context.Authors.Add(author);
       await context.SaveChangesAsync();
       ICheepRepository repo = new CheepRepository(context);
-      
+
       var authorDto = await repo.ReadAuthorByName(name);
       Assert.NotNull(author);
       Assert.Equal(name, authorDto.Name);
    }
-   
+
    [Theory]
-   [InlineData(1,"Tom", "myemail")]
+   [InlineData(1, "Tom", "myemail")]
    public async void TestCreateAuthor(int id, string name, string email)
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
@@ -130,15 +134,16 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-      
+
       ICheepRepository repo = new CheepRepository(context);
       var author = await repo.CreateAuthor(name, email, id);
-      
+
       Assert.NotNull(author);
-      
+
    }
+
    [Theory]
-   [InlineData(1,"Tom", "myemail", 10 )]
+   [InlineData(1, "Tom", "myemail", 10)]
    public async void TestGetAuthorCount(int id, string name, string email, int authorNr)
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
@@ -148,35 +153,67 @@ public class UnitTestCheepRepo : IDisposable
 
       using var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
-      
-     
-      
+
+
+
       ICheepRepository repo = new CheepRepository(context);
 
       for (int i = 0; i < authorNr; i++)
       {
-         await repo.CreateAuthor(name+i, email+i, id+i);
+         await repo.CreateAuthor(name + i, email + i, id + i);
       }
-      
+
       var athcount = await repo.GetAuthorCount();
-      
+
       Assert.Equal(authorNr, athcount);
    }
-   /*
-   [Theory]
-   public async void TestReadCheepText()
+
+   [Fact]
+   public async void TestCreateCheep()
    {
+      //var repo = await UtilFunctionsTest.CreateInMemoryDb();
+      using var connection = new SqliteConnection("Filename=:memory:");
+      await connection.OpenAsync();
+      var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+
+      using var context = new ChirpDBContext(builder.Options);
+      await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+      var author = new Author() { AuthorId = 1, Cheeps = null, Email = "mymail", Name = "Tom" };
+      context.Authors.Add(author);
+      await context.SaveChangesAsync();
+
+      ICheepRepository repo = new CheepRepository(context);
+
+      var cheepDataTransferObject = new CheepDTO
+      {
+         Text = "Something",
+         Author = author,
+         TimeStamp = 1728643569
+      };
       
+      repo.CreateCheep(cheepDataTransferObject);
+      
+      // check that the cheeps is added to the database
    }
 
-   
-   
-   
-   TestGetAuthorCount()
-   
-   TestCreateCheep
-   
-   */
+
+
+
+/*
+[Theory]
+public async void TestReadCheepText()
+{
+
+}
+
+
+
+
+
+
+Testupdatecheep
+
+*/
    
     
 }
