0aea3f373a0f74937f71f67eb949a46628092118
Theresia <theresia.ravn@gmail.com>
2024-11-08 13:25:50 +0100

    no duplicate emails
    Co-authored-by: Adrian <adrianjuul123@gmail.com>

--- src/ChirpWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/ChirpWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -134,7 +134,8 @@ namespace ChirpWeb.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var userExist = await _userManager.FindByEmailAsync(Input.Email);
+                var userExist = await _userManager.FindByNameAsync(Input.Email);
+                
                 if (userExist == null)
                 {
                     //var user = await _service.CreateAuthor(name, "", id);
@@ -148,10 +149,9 @@ namespace ChirpWeb.Areas.Identity.Pages.Account
                     await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                     await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                     user.Email = Input.Email;
-
+                    
                     var result = await _userManager.CreateAsync(user, Input.Password);
-
-
+                    
                     if (result.Succeeded)
                     {
                         _logger.LogInformation("User created a new account with password.");
