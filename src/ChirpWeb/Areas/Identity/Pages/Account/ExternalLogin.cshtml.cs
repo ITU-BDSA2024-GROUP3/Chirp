c80509ece1dac6a8f3476b1c1efe504e9754aa86
Adrian <adrianjuul123@gmail.com>
2024-11-15 13:37:45 +0100

    Added automatic regristration with new github user
    Co-authored-by: Bárður Berg <bardurndberg@gmail.com>

--- src/ChirpWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
+++ src/ChirpWeb/Areas/Identity/Pages/Account/ExternalLogin.cshtml.cs
@@ -155,8 +155,59 @@ namespace ChirpWeb.Areas.Identity.Pages.Account
                     Email = emailClaim?.Value ?? string.Empty, // Default to empty if claim is missing
                     Name = nameClaim?.Value ?? string.Empty   // Default to empty if claim is missing
                 };
+                
+                if (ModelState.IsValid)
+                {
+                var user = CreateUser();
+                
+                user.Cheeps = new List<Cheep>();
+                user.Name = Input.Name;
+                var id = await _service.GetAuthorCount();
+                user.UserId = id + 1;
 
-                return Page();
+                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.Email = Input.Email;
+
+                var res = await _userManager.CreateAsync(user);
+                if (res.Succeeded)
+                {
+                    res = await _userManager.AddLoginAsync(user, info);
+                    if (res.Succeeded)
+                    {
+                        _logger.LogInformation("User created an account using {Name} provider.", info.LoginProvider);
+
+                        var userId = await _userManager.GetUserIdAsync(user);
+                        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                        code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+                        var callbackUrl = Url.Page(
+                            "/Account/ConfirmEmail",
+                            pageHandler: null,
+                            values: new { area = "Identity", userId = userId, code = code },
+                            protocol: Request.Scheme);
+
+                        await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                            $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+                        // If account confirmation is required, we need to show the link if we don't have a real email sender
+                        if (_userManager.Options.SignIn.RequireConfirmedAccount)
+                        {
+                            return RedirectToPage("./RegisterConfirmation", new { Email = Input.Email });
+                        }
+
+                        await _signInManager.SignInAsync(user, isPersistent: false, info.LoginProvider);
+                        return LocalRedirect(returnUrl);
+                    }
+                }
+                foreach (var error in res.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            ProviderDisplayName = info.ProviderDisplayName;
+            ReturnUrl = returnUrl;
+            return Page();
             }
         }
 
