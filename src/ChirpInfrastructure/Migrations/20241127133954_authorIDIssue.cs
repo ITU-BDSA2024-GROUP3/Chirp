52e7d51d93004589187eb485a9968f89f5cbde37
nalm2812 <nalm@itu.dk>
2024-11-27 14:44:24 +0100

    repos are split up
    Co-authored-by: group 2

---
+++ src/ChirpInfrastructure/Migrations/20241127133954_authorIDIssue.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChirpIntegration.Migrations
+{
+    /// <inheritdoc />
+    public partial class authorIDIssue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
