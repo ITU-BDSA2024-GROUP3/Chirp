d57d0d02531b38681e858e219db2554473283cfb
nalm2812 <nalm@itu.dk>
2024-12-06 13:19:18 +0100

    only likeAndUnlikeCheeps doesn't work

--- test/UnitTest/UnitTestCheepRepo.cs
+++ test/UnitTest/UnitTestCheepRepo.cs
@@ -15,33 +15,53 @@ public class UnitTestCheepRepo : IDisposable
    {
    }
 
-   [Fact]
-   public async void TestReadCheep()
+   public async Task<ChirpDBContext> CreateDbContext()
    {
-      //var repo = await UtilFunctionsTest.CreateInMemoryDb();
-      using var connection = new SqliteConnection("Filename=:memory:");
+      var connection = new SqliteConnection("Filename=:memory:");
       await connection.OpenAsync();
       var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-      using var context = new ChirpDBContext(builder.Options);
+      var context = new ChirpDBContext(builder.Options);
       await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+      return context;
+   }
 
-      var author = new Author() { UserId = 1, Cheeps = null, Email = "mymail", Name = "Tom" , FollowingList = new List<int>()};
-
+   public async void AddCheepOne(ChirpDBContext context, Author author)
+   {
       var cheep = new Cheep
       {
          CheepId = 1,
          Author = author,
          UserId = author.UserId,
          Text = "messageData",
-         TimeStamp = DateTimeOffset.FromUnixTimeSeconds(1728643569)
-            .UtcDateTime // Ensure this matches the format in your model
+         TimeStamp = DateTimeOffset.FromUnixTimeSeconds(1728643569).UtcDateTime // Ensure this matches the format in your model
+
       };
+      context.Cheeps.Add(cheep);
+
+      
+   }
+   public async Task<ICheepRepository> CreateInMemoryDb()
+   {
+      var context = await CreateDbContext();
+        
+      var author = new Author() { UserId = 1, Cheeps = null, Email = "mymail", Name = "Tom", FollowingList = new List<int>()};
+        
+      AddCheepOne(context, author);
 
       context.Authors.Add(author);
-      context.Cheeps.Add(cheep);
       await context.SaveChangesAsync();
-      ICheepRepository repo = new CheepRepository(context);
+      return new CheepRepository(context);
+
+      // Act
+      //var result = repository.ReadCheeps(1, 1);
+        
+   }
+   [Fact]
+   public async void TestReadCheep()
+   {
+      
+      ICheepRepository repo = await CreateInMemoryDb();
       var cheeps = repo.ReadCheeps(1, null);
       Assert.NotNull(cheeps);
       var messages = new List<string>();
@@ -58,30 +78,16 @@ public class UnitTestCheepRepo : IDisposable
    public async void TestReadfollowerCheep()
    {
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
-      using var connection = new SqliteConnection("Filename=:memory:");
-      await connection.OpenAsync();
-      var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-      using var context = new ChirpDBContext(builder.Options);
-      await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+      var context = await CreateDbContext();
 
       var author1 = new Author() { UserId = 1, Cheeps = null, Email = "mymail", Name = "Tom" , FollowingList = new List<int>()};
       var author2 = new Author() { UserId = 2, Cheeps = null, Email = "mymaile", Name = "Tommy" , FollowingList = new List<int>()};
       author1.FollowingList.Add(author2.UserId);
       
-      var cheep = new Cheep
-      {
-         CheepId = 1,
-         Author = author2,
-         UserId = author2.UserId,
-         Text = "messageData",
-         TimeStamp = DateTimeOffset.FromUnixTimeSeconds(1728643569)
-            .UtcDateTime // Ensure this matches the format in your model
-      };
+      AddCheepOne(context, author2);
 
       context.Authors.Add(author1);
       context.Authors.Add(author2);
-      context.Cheeps.Add(cheep);
       await context.SaveChangesAsync();
       ICheepRepository repo = new CheepRepository(context);
       var cheeps = repo.ReadFollowedCheeps(1, 1);
@@ -109,13 +115,7 @@ public class UnitTestCheepRepo : IDisposable
    [Fact]
    public async void TestCreateCheep()
    {
-      //var repo = await UtilFunctionsTest.CreateInMemoryDb();
-      using var connection = new SqliteConnection("Filename=:memory:");
-      await connection.OpenAsync();
-      var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-      using var context = new ChirpDBContext(builder.Options);
-      await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+      var context = await CreateDbContext();
       
       //create author and add to database
       var author = new Author() { UserId= 1, Cheeps = null, Email = "mymail", Name = "Tom", FollowingList = new List<int>() };
@@ -139,13 +139,7 @@ public class UnitTestCheepRepo : IDisposable
    [Fact]
    public async void TestUpdateCheep()
    {
-      //var repo = await UtilFunctionsTest.CreateInMemoryDb();
-      using var connection = new SqliteConnection("Filename=:memory:");
-      await connection.OpenAsync();
-      var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
-
-      using var context = new ChirpDBContext(builder.Options);
-      await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+      var context = await CreateDbContext();
       
       //create author and add to database
       var author = new Author() { UserId= 1, Cheeps = null, Email = "mymail", Name = "Tom", FollowingList = new List<int>()};
@@ -185,6 +179,28 @@ public class UnitTestCheepRepo : IDisposable
    [Fact]
    public async void likeandunlikecheeps()
    {
+      //var context = await CreateDbContext();
+
+      //create author and add to database
+      /*var author = new Author() { UserId= 1, Cheeps = null, Email = "mymail", Name = "Tom", FollowingList = new List<int>() };
+      context.Authors.Add(author);
+      await context.SaveChangesAsync();
+      //MAKE THE DATABASE WITH AUTHOR
+      ICheepRepository repo = new CheepRepository(context);
+
+      //dto cheep that will be created by our author
+      var cheepDataTransferObject = new CheepDTO
+      (
+         text: "Something",
+         userId: author.UserId,
+         cheepId: 1,
+         authorName: author.Name,
+         timeStamp: 1728643569
+      );
+
+      await repo.CreateCheep(cheepDataTransferObject);
+      Assert.Equal(1,repo.LikeCheep(1, 1).Result);
+      Assert.Equal(0,repo.UnLikeCheep(1, 1).Result);*/
       //var repo = await UtilFunctionsTest.CreateInMemoryDb();
       using var connection = new SqliteConnection("Filename=:memory:");
       await connection.OpenAsync();
@@ -213,6 +229,8 @@ public class UnitTestCheepRepo : IDisposable
       await repo.CreateCheep(cheepDataTransferObject);
       Assert.Equal(1,repo.LikeCheep(1, 1).Result);
       Assert.Equal(0,repo.UnLikeCheep(1, 1).Result);
+
+
 
 
    }
